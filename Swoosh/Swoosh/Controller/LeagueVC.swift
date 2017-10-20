14a17a986efced9a88695b94f2f6e4ce270b775c
adam mcbride <adam.mcbride@gmail.com>
2017-10-20 01:43:27 -0400

    created IBActions for the buttons, created a model for the player and in struct, created a function to handle selecting options from multiple buttons

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -9,14 +9,35 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectleague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectleague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectleague(leagueType: "coed")
+    }
+    
+    func selectleague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
 }
